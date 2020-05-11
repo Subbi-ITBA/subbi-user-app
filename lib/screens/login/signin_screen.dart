d47e579ecb72ea216e498125b5debbdc11ced5b2
Matias Julian Apablaza <mapablaza@itba.edu.ar>
2020-05-11 20:25:52 -0300

    - Email/password login option added

--- lib/screens/login/signin_screen.dart
+++ lib/screens/login/signin_screen.dart
@@ -1,51 +1,67 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:subbi/models/user.dart';
+import 'package:subbi/screens/login/signin_boxes/email_signin_box.dart';
+import 'package:subbi/screens/login/signin_boxes/google_signin_box.dart';
 import 'package:subbi/screens/login/signup_screen.dart';
+
+BuildContext screenContext;
 
 class SigninScreen extends StatelessWidget{
 
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+  
+    screenContext = context;
 
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
 
       body: Center(
-        child: Container(
-          height: 300,
-          width: 300,
-          child: Card(
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
+        child: SingleChildScrollView(
+                  child: Container(
+            height: 500,
+            width: 300,
+            child: Card(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
 
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset('assets/logo.png', height: 70, width: 70),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Welcome',
-                      style: Theme.of(context).textTheme.display1,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('assets/logo.png', height: 70, width: 70),
                     ),
-                  ),
 
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: GoogleSignInBox(signInButtonHeight: 40),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Welcome',
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                    ),
 
-                ]
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: EmailSignInBox(
+                        parentCallback: signIn,
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GoogleSignInBox(
+                        parentCallback: signIn,
+                      ),
+                    ),
+
+                  ]
+                ),
               ),
             ),
           ),
@@ -56,147 +72,22 @@ class SigninScreen extends StatelessWidget{
 
   }
 
-
-}
-
-
-
-
-class GoogleSignInBox extends StatefulWidget {
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final double signInButtonHeight;
-
-  GoogleSignInBox({@required this.signInButtonHeight});
-
-  @override
-  GoogleSignInBoxState createState() => GoogleSignInBoxState();
-
-}
-
-
-
-class GoogleSignInBoxState extends State<GoogleSignInBox> {
-
-
-  /* ----------------------------------------------------------------------------
-                               STATIC VALUES
-  ---------------------------------------------------------------------------- */
-
-  static const _BUTTON_COLOR = Color(0xFF4285f4);
-  static const _BUTTON_LOGO_ASSET = 'assets/google_signin.png';
-  static const _ERROR_TEXT = 'Lo sentimos, se produjo un error, por favor verifica tu conexión a Internet.';
-
-  static const _AUTH_SCOPES = ['https://www.googleapis.com/auth/user.birthday.read', 'https://www.googleapis.com/auth/userinfo.profile'];
-
-
-  /* ----------------------------------------------------------------------------
-                                   STATE
-  ---------------------------------------------------------------------------- */
-
-  String state;
-
-
-  /* ----------------------------------------------------------------------------
-    Display Signin button with loading and error indicators
-  ---------------------------------------------------------------------------- */
-
-  @override
-  Widget build(BuildContext context) {
-
-    switch(state){
-      
-      // LOADING INDICATOR
-
-      case 'loading':
-        return CircularProgressIndicator();
-        break;
-
-
-      // ERROR INDICATOR
-
-      case 'error':
-        return Row(children: <Widget>[
-          Icon(Icons.close, color: Colors.red),
-          Flexible(child: Text(_ERROR_TEXT, maxLines: 2,)),
-        ]);
-        break;
-
-
-      // LOGIN SCREEN
-
-      default:
-        return GestureDetector(
-          onTap: (){
-
-            signIn(context).catchError((error){
-              setState(() { this.state='error'; });
-            });
-
-            setState(() { this.state='loading'; });
-            
-          },
-          child: Container(
-            height: widget.signInButtonHeight,
-            width: widget.signInButtonHeight*4.5,
-            color: _BUTTON_COLOR,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children:[
-                Image.asset(_BUTTON_LOGO_ASSET),
-                Text('Sign in with Google', style: Theme.of(context).textTheme.caption.copyWith(color: Colors.white))
-              ]
-            ),
-          )
-        );
-
-    }
-  }
-
-
-  /* ----------------------------------------------------------------------------
-    Perform Google Signin and store credentials. If user exists, return to main
-    app, else go to PersonalityAssesment.
-  ---------------------------------------------------------------------------- */
-
-  Future<void> signIn(context) async {
-
-    // Get credentials
-
-    GoogleSignInAccount googleUser;
-    try{
-      googleUser = await GoogleSignIn(scopes: _AUTH_SCOPES).signIn();
-    }catch(e){
-      print(e.toString());
-    }
-
-
-    // Create FirebaseAuth user
-
-    AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: (await googleUser.authentication).accessToken,
-      idToken: (await googleUser.authentication).idToken,
-    );
-    FirebaseUser fbUser = (await widget._auth.signInWithCredential(credential)).user;
-
+  
+  void signIn(FirebaseUser fbUser) async{
 
     // Initialize user
-
-    User user = Provider.of<User>(context, listen: false);
+    User user = Provider.of<User>(screenContext, listen: false);
     user.initialize(fbUser);
 
-
     // If user is new, go to sign up screen
-
     bool exists = await user.signIn();
-
     if(!exists){
-      await Navigator.of(context).push(
+      await Navigator.of(screenContext).push(
         MaterialPageRoute<void>(builder: (BuildContext context) => SignupScreen())
       );
     }
 
-    Navigator.pop(context, fbUser);
+    Navigator.pop(screenContext, fbUser);
 
   }
 
