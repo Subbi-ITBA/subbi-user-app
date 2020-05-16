a5ca5d485c334f108aa99effd33806ef53b3896e
Matias Julian Apablaza <mapablaza@itba.edu.ar>
2020-05-16 00:01:13 -0300

    Changed get methods of auction and profile models

--- lib/models/auction/auction.dart
+++ lib/models/auction/auction.dart
@@ -7,14 +7,37 @@ class Auction{
 
   String title;
   String imagesURL;
-  Profile owner;
   DateTime deadLine;
-  List<Bid> bids;
+  String ownerUid;
 
-  Auction({@required this.title, @required this.imagesURL, @required this.owner, @required this.deadLine, @required this.bids});
+  List<Bid> _bids;
+
+
+  Auction({@required owner, @required this.title, @required this.imagesURL, @required this.deadLine, @required this.ownerUid});
 
   Future<void> post() => throw UnimplementedError();
 
   Future<void> delete() => throw UnimplementedError();
+
+
+  /* ------------------------------------------------------------
+    Fetches bids if they haven't been fetched already
+  ------------------------------------------------------------ */
+
+  Future<List<Bid>> get bids async{
+
+    if(_bids==null);
+      // Fetch bids from server
+
+    return _bids;
+
+  }
+
+
+  Stream<Bid> subscribeToBids() => throw UnimplementedError();
+
+
+  Future<Profile> get owner => throw UnimplementedError();
+
 
 }
