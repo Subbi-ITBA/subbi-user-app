7b07c4b96175e66e92e5914128066bed600a627d
MatiMonaco <matimonaco@outlook.com>
2020-06-02 10:17:15 -0300

    Co-authored-by: Scott Lin <scott.lin099@gmail.com>

---
+++ lib/screens/tabs/auction_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Auction extends StatefulWidget {
+  @override
+  _AuctionState createState() => _AuctionState();
+}
+
+class _AuctionState extends State<Auction> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
