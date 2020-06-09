b482b4bf9a8031f542909f89912d01f16b7baa03
MatiMonaco <matimonaco@outlook.com>
2020-06-09 12:06:31 -0300

    update

--- lib/screens/tabs/auction_screen.dart
+++ lib/screens/tabs/auction_screen.dart
@@ -5,13 +5,12 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 class AuctionScreen extends StatelessWidget {
   Map data = {};
-  Auction auction;
 
   @override
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
-    this.auction = this.data['auction'];
-    Size size = MediaQuery.of(context).size;
+    Auction auction = this.data['auction'];
+    // Size size = MediaQuery.of(context).size;
 
     return Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
@@ -41,7 +40,7 @@ class AuctionScreen extends StatelessWidget {
           //       onPressed: () => Navigator.pop(context)),
           // ],
         ),
-        body: Body(auction: this.auction));
+        body: Body(auction: auction));
   }
 }
 
@@ -76,11 +75,11 @@ class Body extends StatelessWidget {
                     padding: const EdgeInsets.fromLTRB(10, 10, 0, 3),
                     child: Text(
                       this.auction.title,
-                      overflow: TextOverflow.ellipsis,
+                      softWrap: true,
                       style: TextStyle(
                           color: Theme.of(context).primaryColor,
                           fontWeight: FontWeight.bold,
-                          fontSize: 24),
+                          fontSize: 18),
                     ),
                   ),
                 ],
@@ -92,35 +91,205 @@ class Body extends StatelessWidget {
             top: 260,
             child: Align(
                 alignment: Alignment.topCenter,
-                child: Timer(auction: this.auction)))
+                child: AuctionInfo(auction: this.auction)))
       ],
     );
   }
 }
 
-class Timer extends StatelessWidget {
+class AuctionInfo extends StatelessWidget {
   final Auction auction;
-  Timer({@required this.auction});
+  AuctionInfo({@required this.auction});
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      width: size.width * 0.8,
-      height: 75,
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0.0, 1.0), //(x,y)
-              blurRadius: 4.0,
+        width: size.width * 0.9,
+        height: 75,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0.0, 1.0), //(x,y)
+                blurRadius: 4.0,
+              ),
+            ],
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            )),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              child: Text(this.auction.printHighestBid(),
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold)),
             ),
+            Container(child: DeadlineTimer(auction: this.auction)),
           ],
-          color: Colors.white,
-          borderRadius: BorderRadius.all(
-            Radius.circular(5),
-          )),
-    );
+        ));
+  }
+}
+
+class DeadlineTimer extends StatelessWidget {
+  final Auction auction;
+  DeadlineTimer({@required this.auction});
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: Stream.periodic(Duration(seconds: 1), (i) => i),
+        builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+          Duration leftingTime =
+              this.auction.deadLine.difference(DateTime.now());
+          int days = leftingTime.inDays;
+          int seconds = leftingTime.inSeconds.remainder(60);
+          int hours = leftingTime.inHours.remainder(24);
+          int minutes = leftingTime.inMinutes.remainder(60);
+          String daysSring = days < 10 ? "0$days" : "$days";
+          String minutesString = minutes < 10 ? "0$minutes" : "$minutes";
+          String hoursString = hours < 10 ? "0$hours" : "$hours";
+          String secondsString = seconds < 10 ? "0$seconds" : "$seconds";
+
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(children: <Widget>[
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Theme.of(context).accentColor),
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      daysSring,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  "Days",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Theme.of(context).accentColor, fontSize: 12),
+                )
+              ]),
+              Padding(
+                padding: EdgeInsets.fromLTRB(7, 0, 6, 15),
+                child: Text(
+                  ":",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Theme.of(context).accentColor),
+                      child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            hoursString,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ))),
+                  Text(
+                    "Hours",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 12),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(6, 0, 1, 15),
+                child: Text(
+                  ":",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Theme.of(context).accentColor),
+                      child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            minutesString,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ))),
+                  Text(
+                    "Minutes",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 12),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                child: Text(
+                  ":",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Theme.of(context).accentColor),
+                      child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            secondsString,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ))),
+                  Text(
+                    "Seconds",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 12),
+                  )
+                ],
+              ),
+            ],
+          );
+        });
   }
 }
 
