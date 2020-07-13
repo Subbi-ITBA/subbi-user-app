85d8b0d70c2c86ff871d1f2cf3b08361f38cd6fd
Matías Julián Apablaza <mapablaza@itba.edu.ar>
2020-07-13 14:53:31 -0300

    Fixed profile name issues in chat

--- lib/screens/tabs/chat_screen.dart
+++ lib/screens/tabs/chat_screen.dart
@@ -42,7 +42,9 @@ class _ChatScreenState extends State<ChatScreen> {
               ChatMessage(
                 text: data["msg"],
                 createdAt: DateTime.parse(data["date"]).toLocal(),
-                user: ChatUser(),
+                user: ChatUser(
+                  name: widget.withProfile.name,
+                ),
               ),
             );
           },
